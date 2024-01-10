3be92a7a99f48505163d840f7292e75664b590c7
Phakapongbomb <spybomb2008@gmail.com>
2024-01-10 15:19:29 +0700

    Add OGP, image & semantic tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
-    <div>
-      <Homepage></Homepage>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
